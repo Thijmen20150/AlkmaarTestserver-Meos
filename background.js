56f332fa60efccee370f28dec64981e988d07bcf
Thijmen20150 <Thijmenkuiper00@gmail.com>
2025-06-19 16:38:58 +0200

    Achtergond fix

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // Voeg hier je achtergrondafbeeldingen toe
 const backgrounds = [
-    'https://cdn.discordapp.com/attachments/1378245756179185776/1378245817822871595/Kmar_1.png?ex=683be6f1&is=683a9571&hm=e6f5174dea07df12cdea695fb17f13385ed506c103cd3b617319e18c4242fca5&',
-    // Voeg eventueel meer afbeeldingen toe
+    'https://cdn.discordapp.com/attachments/1381981725307174942/1385267404564856863/Dienst.png?ex=6855724e&is=685420ce&hm=29e33226d566ee50e8e37a0a48610fd3327a0cc95e6b01ed466f00faf06daeb7&',
+    'https://cdn.discordapp.com/attachments/1381981725307174942/1385267405303058572/Kmar_1.png?ex=6855724e&is=685420ce&hm=51ca95b25d7c04ed1a0059f94c9b5f2c8889f7eb9fba24b719a0fcade6741654&'    // Voeg eventueel meer afbeeldingen toe
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
